0dc5f929d3b004bd7573137c116e65a2b5186cb0
Purvaja22 <58504578+Purvaja22@users.noreply.github.com>
2019-12-04 13:28:48 +0530

    Add files via upload

---
+++ E6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class E6
+{
+	public static void main(String args[])
+	{
+		int num;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter a number");
+			num=s.nextInt();
+		if(num%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+}
+		
